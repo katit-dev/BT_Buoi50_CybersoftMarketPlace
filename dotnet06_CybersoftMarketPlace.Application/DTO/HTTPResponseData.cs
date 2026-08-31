5881a75ed72108eb6797905755f0ccedfd34c2b3
khanhvy0908 <khanhvy0946265560@gmail.com>
2026-08-31 22:38:07 +0700

    Them Messages cho nghiep vu update profile

--- dotnet06_CybersoftMarketPlace.Application/DTO/HTTPResponseData.cs
+++ dotnet06_CybersoftMarketPlace.Application/DTO/HTTPResponseData.cs
@@ -12,10 +12,40 @@ public class HTTPResponseData<T>
 
 public static class UserResponseMessageDTO
 {
-    public const string SuccessRegister = "User registered successfully.";
-    public const string FailedRegister = "User registration failed.";
-    public const string EmailUsernameOrPhoneExists = "Email, username, or phone already exists.";
-    public const string SuccessLogin = "User logged in successfully.";
-    public const string InvalidLogin = "Invalid email, username, phone, or password.";
-    public const string UserNotFound = "Username/Phone/Email or password is incorrect.";
+    // Register
+    public const string SuccessRegister =
+        "User registered successfully.";
+
+    public const string FailedRegister =
+        "User registration failed.";
+
+    public const string EmailUsernameOrPhoneExists =
+        "Email, username, or phone already exists.";
+
+
+    // Login
+    public const string SuccessLogin =
+        "User logged in successfully.";
+
+    public const string InvalidLogin =
+        "Invalid email, username, phone, or password.";
+
+
+    // User
+    public const string UserNotFound =
+        "Username/Phone/Email or password is incorrect.";
+
+    public const string InvalidToken =
+        "Invalid token.";
+
+
+    // Profile
+    public const string GetProfileSuccess =
+        "Get profile successfully.";
+
+    public const string UpdateProfileSuccess =
+        "Update profile successfully.";
+
+    public const string UpdateProfileFailed =
+        "Update profile failed.";
 }
